ede1bb6415142ff6eace31ab3a37f476e8877ba4
AndyTheDragon <andre.jansam@gmail.com>
2024-10-11 23:30:17 +0200

    Added boilerplate code

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,33 @@
 package app;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import app.config.SessionConfig;
+import app.config.ThymeleafConfig;
+import app.persistence.ConnectionPool;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinThymeleaf;
+
+public class Main
+{
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "postgres";
+    private static final String URL = "jdbc:postgresql://localhost:5432/%s?currentSchema=public";
+    private static final String DB = "olsker_cupcake";
+
+    private static final ConnectionPool connectionPool = ConnectionPool.getInstance(USER, PASSWORD, URL, DB);
+
+
+    public static void main(String[] args)
+    {
+        // Initializing Javalin and Jetty webserver
+
+        Javalin app = Javalin.create(config -> {
+            config.staticFiles.add("/public");
+            config.jetty.modifyServletContextHandler(handler ->  handler.setSessionHandler(SessionConfig.sessionConfig()));
+            config.fileRenderer(new JavalinThymeleaf(ThymeleafConfig.templateEngine()));
+        }).start(7070);
+
+        // Routing
+
+        app.get("/", ctx ->  ctx.render("index.html"));
     }
 }

fd8b6a3b3e23afd87cb209e59364f522695cce73
Lasse Jensen <Lasse.Erik.Jensen@mensa.dk>
2024-10-18 21:52:38 +0200

    Made the User Class, and gave it getters + deposit and buy method. Deposit and buy are blank for now

---
+++ src/main/java/app/entities/User.java
@@ -0,0 +1,45 @@
+package app.entities;
+
+public class User {
+    private int userId;
+    private String username;
+    private String password;
+    private String role;
+    private int balance;
+
+    public User(int userId, String username, String password, String role, int balance) {
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.role = role;
+        this.balance = balance;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void deposit(){
+
+    }
+
+    public void buy(){
+
+    }
+}

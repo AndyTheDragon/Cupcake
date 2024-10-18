3e76f81131c674a494b53c84e103e99ea7d8e79b
Lasse Jensen <Lasse.Erik.Jensen@mensa.dk>
2024-10-18 21:55:46 +0200

    Added the OrderLine class, with getters.

---
+++ src/main/java/app/entities/OrderLine.java
@@ -0,0 +1,31 @@
+package app.entities;
+
+public class OrderLine {
+    private int orderId;
+    private int quantity;
+    private Cupcake cupcake;
+    int price;
+
+    public OrderLine(int orderId, int quantity, Cupcake cupcake, int price) {
+        this.orderId = orderId;
+        this.quantity = quantity;
+        this.cupcake = cupcake;
+        this.price = price;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public Cupcake getCupcake() {
+        return cupcake;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+}

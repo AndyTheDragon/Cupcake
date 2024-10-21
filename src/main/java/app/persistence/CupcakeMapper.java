2438273a66477f89715fc339bc238199ddb2e025
AndyTheDragon <andre.jansam@gmail.com>
2024-10-21 15:25:49 +0200

    CupcakeMapper fetch from database

--- src/main/java/app/persistence/CupcakeMapper.java
+++ src/main/java/app/persistence/CupcakeMapper.java
@@ -2,17 +2,47 @@ package app.persistence;
 
 import app.entities.CupcakeFlavour;
 import app.entities.CupcakeType;
+import app.exceptions.DatabaseException;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class CupcakeMapper
 {
 
-    public static List<CupcakeFlavour> getFlavours(CupcakeType cupcakeType, ConnectionPool dbConnection)
+    public static List<CupcakeFlavour> getFlavours(CupcakeType cupcakeType, ConnectionPool dbConnection) throws DatabaseException
     {
         List<CupcakeFlavour> flavours = new ArrayList<>();
-        flavours.add(new CupcakeFlavour(1,500,"Chokolade","Chokolade",CupcakeType.TOP));
+        String sql;
+        if (cupcakeType == CupcakeType.BOTTOM)
+        {
+            sql = "SELECT * FROM cupcake_flavours WHERE is_bottom_flavour = TRUE";
+        }
+        else //dvs. (cupcakeType == CupcakeType.TOP)
+        {
+            sql = "SELECT * FROM cupcake_flavours WHERE is_top_flavour = TRUE";
+        }
+        try (Connection con = dbConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql))
+        {
+            ResultSet rs = ps.executeQuery();
+            while (rs.next())
+            {
+                flavours.add(new CupcakeFlavour(rs.getInt("flavour_id"),
+                                                rs.getInt("price"),
+                                                rs.getString("flavour_name"),
+                                                rs.getString("flavour_name"),
+                                                cupcakeType));
+            }
+        } catch (SQLException e)
+        {
+            throw new DatabaseException(e.getMessage());
+        }
+
         return flavours;
     }
 }

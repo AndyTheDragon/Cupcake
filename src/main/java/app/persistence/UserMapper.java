7eeb925127e775c84ed7ae72220aa5f1644bbf00
Avz <68717428+montif16@users.noreply.github.com>
2024-10-23 13:21:22 +0200

    SQL code clarification
    Changed SQL text to upper case

--- src/main/java/app/persistence/UserMapper.java
+++ src/main/java/app/persistence/UserMapper.java
@@ -31,7 +31,7 @@ public class UserMapper {
 
     public static User login(String username, String password, ConnectionPool connectionPool) throws DatabaseException {
 
-        String sql = "Select * from \"users\" where username=? and password=?";
+        String sql = "Select * FROM \"users\" WHERE username=? and password=?";
         try (Connection connection = connectionPool.getConnection()) {
             try (PreparedStatement ps = connection.prepareStatement(sql)) {
                 ps.setString(1, username);

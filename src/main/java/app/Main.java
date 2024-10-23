d551499436ab03db88758a62739aebfd615a2c05
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-10-23 13:24:03 +0200

    Main oprettet en route

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -2,6 +2,7 @@ package app;
 
 import app.config.SessionConfig;
 import app.config.ThymeleafConfig;
+import app.controllers.OrderController;
 import app.persistence.ConnectionPool;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinThymeleaf;
@@ -28,6 +29,6 @@ public class Main
 
         // Routing
 
-        app.get("/", ctx ->  ctx.render("ordrehistory.html"));
+        OrderController.addRoutes(app, connectionPool);
     }
 }

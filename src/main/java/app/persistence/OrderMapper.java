d4573fa48c7420955196b5ce09c7c6dc4df8a1c4
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2024-10-23 12:51:19 +0200

    opdateret getmetode i orderMapper

--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -13,12 +13,14 @@ public class OrderMapper {
 
 
         public static Order getOrder(ConnectionPool pool){
-            String sql = "SELECT order_id, name, status FROM orders ORDER BY date_placed ASC";
+            String sql = "SELECT order_id, name, date_placed, date_paid, date_completed, status, user_id FROM orders ORDER BY date_placed ASC\n";
             int order_id;
             String name;
             Date date_placed;
             Date date_paid;
             Date date_completed;
+            String status;
+            int user_id;
 
 
             try (Connection connection = pool.getConnection())
@@ -33,13 +35,19 @@ public class OrderMapper {
                         date_placed = rs.getDate("Date placed");
                         date_paid = rs.getDate("Date paid");
                         date_completed = rs.getDate("Date completed");
-                        return new Order(order_id, name, date_placed, date_paid, date_completed);
+                        status = rs.getString("Status");
+                        user_id = rs.getInt("User_id");
+                        return new Order(order_id, name, date_placed, date_paid, date_completed,status, user_id);
                     }
                 }
             } catch (SQLException e)
             {
+
                 throw new DatabaseException(e.getMessage());
+
+
             }
+
             return null;
 
         }

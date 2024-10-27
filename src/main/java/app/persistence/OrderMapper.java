86bb7596379c913825bc22fb555c2d74842612a6
AndyTheDragon <andre.jansam@gmail.com>
2024-10-27 20:24:49 +0100

    fixing warnings

--- src/main/java/app/persistence/OrderMapper.java
+++ src/main/java/app/persistence/OrderMapper.java
@@ -7,10 +7,6 @@ import java.sql.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class OrderMapper
 {
